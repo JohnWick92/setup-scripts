52bafd2376ab09adf63641fe8c005c9282ecded1
JohnWick92 <grumec92@gmail.com>
2024-05-01 23:21:11 -0300

    Fixing sudo on fish.sh and wsl asdf

--- rhel-based-fish.sh
+++ rhel-based-fish.sh
@@ -5,7 +5,7 @@ asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin add java https://github.com/halcyon/asdf-java.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
-apt install -y autoconf patch libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+sudo apt install -y autoconf patch libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
 asdf install ruby 3.2.3 && asdf global ruby 3.2.3
 asdf install java openjdk-17 && asdf global java openjdk-17
 asdf install nodejs latest && asdf global nodejs latest
--- ubuntu-wsl.sh
+++ ubuntu-wsl.sh
@@ -17,14 +17,13 @@ install_base_dev() {
 }
 
 install_rust_alternatives() {
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 	cargo install ripgrep zoxide fd-find tealdeer procs git-delta bat exa du-dust tokei ytop rmesg grex
 	echo "zoxide init fish | source" >>~/.config/fish/config.fish
 }
 
 install_asdf() {
 	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-	sed '1 s/^/source ~/.asdf/asdf.fish\n/' ~/.config/fish/config.fish
+	echo 'source ~/.asdf/asdf.fish' | cat - ~/.config/fish/config.fish >temp && temp ~/.config/fish/config.fish
 	mkdir -p ~/.config/fish/completions
 	and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 }
@@ -36,16 +35,15 @@ install_starship() {
 
 install_lazyvim() {
 	cd /tmp/
-	git clone https://github.com/neovim/neovim && cd neovim
+	git clone https://github.com/neovim/neovim
+	cd neovim
 	make CMAKE_BUILD_TYPE=Release
-	make install
+	sudo make install
 	git clone https://github.com/JohnWick92/my-lazyvim ~/.config/nvim/
 }
 
 last_things() {
 	echo "To export path to fish enter in fish shell and run this commands:"
-	echo "fish_add_path ~/.local/bin"
-	echo "fish_add_path ~/.cargo/bin"
 	echo "To fish be your default shell you need to reboot the machine"
 	echo "Open neovim wait until it download the plugins close and run it again"
 	echo "See the git-delta to set up delta to your git config"
@@ -55,9 +53,9 @@ last_things() {
 check_root
 fetch_updates
 install_base_dev
-install_asdf
-./ubuntu-wsl-fish.sh
 install_rust_alternatives
 install_starship
 install_lazyvim
+install_asdf
+./ubuntu-wsl-fish.sh
 last_things
